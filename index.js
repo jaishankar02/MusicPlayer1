6fa94b7d567bf08541a1057b50486a9b938e19d9
JaiShankar <jaishankar.a1603@gmail.com>
2022-06-25 23:32:14 +0530

    my music player

---
+++ index.js
@@ -0,0 +1,9 @@
+function jai(b){
+    var a=document.getElementById('myplayer');
+    a.src=b.value;
+
+}
+var buttonRight=getElementById('nextbutton')
+buttonRight.onclick = function () {
+    document.getElementById('LatestHin').scrollLeft += 20;
+  };
